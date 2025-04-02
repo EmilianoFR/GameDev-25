22029ae752fcbf53ab919622d6e101effbb2f38d
EmilianoFR <79602333+EmilianoFR@users.noreply.github.com>
2025-04-01 22:35:02 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+var active = ("y");
+while (active == ("y"))
+{Console.WriteLine("Enter first number to be averaged:  ");
+    int Var1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter second number to be averaged:  ");
+    int Var2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter final number to be averaged:  ");
+    int Var3 = int.Parse(Console.ReadLine());
+
+int sum = Var1 + Var2 + Var3;
+int avg = sum / 3;
+
+    Console.WriteLine("Average is: " + avg);
+    Console.WriteLine("Enter 'y' for to restart, or anything else to end: ");
+    active = Console.ReadLine();
+    
+}
